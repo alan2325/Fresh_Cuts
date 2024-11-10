1e5808a7ccfdd91467ac3b5692ad77edd4b3f325
Aiswarya2316 <aiswaryapinkley@gmail.com>
2024-11-10 21:00:46 +0530

    feedback

--- fresh_cuts/app/views.py
+++ fresh_cuts/app/views.py
@@ -150,7 +150,7 @@ def delregister(req):
 
 def userhome(req):
     if 'user' in req.session:
-        data = Product.objects.all()
+        data = Product.objects.all().order_by('-shop')[:4]
         data1 = Buy.objects.filter(user=get_usr(req)).order_by('-date_of_buying')[:2]  # Only get the latest 2 orders
         data2 = cart.objects.filter(user=get_usr(req)).order_by('-id')[:2]  # Get the latest 4 cart items
 
